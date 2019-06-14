b1377a3530d5803f294cc1198042b58cc3fb312e
nahlmb <nahlultra@gmail.com>
2019-06-14 07:39:14 +0700

    back intent fix 3

--- app/src/main/java/com/nahl/berita/Main/MainContract.java
+++ app/src/main/java/com/nahl/berita/Main/MainContract.java
@@ -9,13 +9,11 @@ public interface MainContract {
     interface View {
         void showLoading();
         void hideLoading();
-        void showFragmentList(Boolean everyThing, Boolean headLines, String params1, String shortBy);
         void showUserNameAndDate(String userName, String date);
         void showCountryTopHeadline(List<News.Article> articlesItems);
         void showCategoryTopHeadline(List<News.Article> articlesItems);
         void showTopicUserLike(List<News.Article> articlesItems);
         void setFavoritTopic();
-        void notYetMessage();
     }
 
     interface Presenter {
